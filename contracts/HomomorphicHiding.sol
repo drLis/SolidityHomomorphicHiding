8a8f1ec13b40377e02bee06dacf6dfe69c03b1db
unknown <dr.lis@outlook.com>
2020-03-25 21:19:38 +0300

    Change library type to pure struct

--- contracts/HomomorphicHiding.sol
+++ contracts/HomomorphicHiding.sol
@@ -1,7 +1,15 @@
-pragma solidity ^0.5.0;
+pragma solidity ^0.6.0;
 
+import "./_lib/elliptic-curve-solidity/contracts/curves/EllipticCurve.sol";
 
-contract HomomorphicHiding
+struct E
 {
-    
+	uint x;
+	uint y;
+	/*
+	function e(uint value) public pure returns (struct E)
+	{
+		E ee;
+		return ee;
+	}*/
 }
