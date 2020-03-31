d31a60c287f1ed49e58aa76d2feacf657d02693c
unknown <dr.lis@outlook.com>
2020-03-31 18:26:12 +0300

    Equals completed

--- contracts/HomomorphicHiding.sol
+++ contracts/HomomorphicHiding.sol
@@ -14,7 +14,7 @@ library HomomorphicHiding
 	{
 		(hh.x, hh.y) = EllipticCurve.ecMul(x, g_x, g_y, a, p);
 	}
-/*
+
 	function add(E memory e1, E memory e2) internal pure returns (E memory hh)
 	{
 
@@ -22,13 +22,13 @@ library HomomorphicHiding
 
 	function equals(E memory e1, E memory e2) internal pure returns (bool)
 	{
-
+		return e1.x == e2.x && e1.y == e2.y;
 	}
 
 	function mul(uint k, E memory e) internal pure returns (E memory hh)
 	{
 
-	}*/
+	}
 
 	uint internal constant g_x = 0x79BE667EF9DCBBAC55A06295CE870B07029BFCDB2DCE28D959F2815B16F81798;
 	uint internal constant g_y = 0x483ADA7726A3C4655DA4FBFC0E1108A8FD17B448A68554199C47D08FFB10D4B8;
